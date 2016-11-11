1ff03d8fe8df8aa894b1687459644ff93bf2a4f2
Karen MacPherson <karen@fullstackacademy.com>
2016-11-11 18:27:01 -0500

    SingleColor component test.

---
+++ components/single-color.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import chai from 'chai'
+chai.use(require('chai-enzyme')())
+
+import SingleColor from './single-color'
+
+describe('<SingleColor />', () => {
+  it('has a background-color of props.color', () => {
+    const color = shallow(<SingleColor color='blue' />)
+    expect(color).to.have.style('backgroundColor', 'blue')
+  })
+})
